3cdac2faa4683a6a2d727c10798d65d5cffc66c7
Gosst4 <valishinstas@gmail.com>
2021-09-15 21:51:01 +0200

    folder renamed

---
+++ Globe/Assets/Scripts/Spawner.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Ball ballPrefab;
+    [SerializeField] private Transform parent;
+    [SerializeField] private int startingNumber;
+
+}
